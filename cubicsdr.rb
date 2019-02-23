10fd36f742fd834b079d57bf96311b98d9119848
xlfe <felixb@gmail.com>
2019-02-23 21:46:54 +1100

    bump cubicsdr

--- cubicsdr.rb
+++ cubicsdr.rb
@@ -2,20 +2,19 @@ require "formula"
 
 class Cubicsdr < Formula
   homepage "https://github.com/cjcliffe/CubicSDR"
-  url "https://github.com/cjcliffe/CubicSDR/archive/0.2.0-beta-rc5.tar.gz"
-  version "0.2.0b5"
-  sha256 "20f9cfeb24717a177b1d57692c54985d16d576fcbed97e9a9415a8bfa9a5510d"
+  url "https://github.com/cjcliffe/CubicSDR/archive/0.2.5.tar.gz"
+  sha256 "5cb44c110fcbbb70a468b7fa402cf35f84d8901b3dd42d471a90ac3f5db00f4d"
   head "https://github.com/cjcliffe/CubicSDR.git"
 
   depends_on "cmake" => :build
 
   option "with-digital", "Build with Digital Lab support"
 
-  depends_on "fftw"
+  depends_on "fftw" => "without-fortran"
   depends_on "librtlsdr"
   depends_on "liquid-dsp"
   depends_on "pothosware/pothos/soapysdr"
-  depends_on "wxwidgets"
+  depends_on "wxmac""
 
   def install
     mkdir "build" do
